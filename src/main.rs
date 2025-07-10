a128efb2fad8a7f6644c0840ff7035580b435523
Tommi Mäklin <tommi@maklin.fi>
2025-07-10 16:19:20 +0200

    Write block headers.

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,15 @@
 // the MIT license, <LICENSE-MIT> or <http://opensource.org/licenses/MIT>,
 // at your option.
 //
-use std::io::Write;
+use std::io::{Read, Write};
 use std::ops::Range;
 use std::path::PathBuf;
 
 use bitpacking::{BitPacker8x, BitPacker};
 use bincode::encode_into_std_write;
+use bincode::decode_from_std_read;
+use bincode::{Encode, Decode};
+use bincode::decode_from_slice;
 use clap::Parser;
 use log::info;
 use needletail::Sequence;
@@ -28,6 +31,21 @@ use dsi_bitstream::prelude::*;
 
 // Command-line interface
 mod cli;
+
+#[derive(Encode, Decode)]
+struct BlockHeader {
+    pub block_size: u32,
+    pub num_records: u32,
+    pub num_u64: u32,
+    pub encoded_size: u32,
+    pub rice_param: u8,
+    pub bitpacker_exponent: u8,
+    pub placeholder1: u64,
+    pub placeholder2: u32,
+    pub placeholder3: u16,
+    // TODO should store the used codec so others can be used
+    // TODO store total number of bases?
+}
 
 /// Initializes the logger with verbosity given in `log_max_level`.
 fn init_log(log_max_level: usize) {
@@ -155,6 +173,9 @@ fn main() {
 
             let (sbwt, lcs) = kbo::index::load_sbwt(index_prefix.as_ref().unwrap());
 
+            // TODO should use MB here
+            let block_size = 65536;
+
             info!("Encoding fastX data...");
             match sbwt {
                 SbwtIndexVariant::SubsetMatrix(sbwt) => {
@@ -162,16 +183,16 @@ fn main() {
 
                     let mut reader = needletail::parse_fastx_file(query_file).unwrap_or_else(|_| panic!("Expected valid fastX file"));
                     let mut u64_encoding: Vec<u64> = Vec::new();
-                    let mut i = 0;
+                    let mut num_records = 0;
 
                     while let Some(rec) = read_from_fastx_parser(&mut *reader) {
                         let seqrec = rec.normalize(true);
                         let res: Vec<(usize, Range<usize>)> = index.matching_statistics(&seqrec);
+                        num_records += 1;
 
                         let mut bases = 0;
                         res.iter().rev().for_each(|matches| {
                             if bases == 0 {
-                                i += 1;
                                 bases = matches.0;
                                 let mut arr: [u8; 8] = [0; 8];
                                 arr[0..4].copy_from_slice(&(matches.1.start as u32).to_ne_bytes());
@@ -180,45 +201,68 @@ fn main() {
                             }
                             bases -= 1;
                         });
+
+                        if u64_encoding.len() > block_size {
+
+                            // Best: Rice
+                            let inv_mean: f64 = ((u64_encoding.len() as f64).ln() - (u64_encoding.iter().sum::<u64>() as f64).ln()).exp();
+                            let param = dsi_bitstream::codes::rice::log2_b(inv_mean);
+
+                            let word_write = MemWordWriterVec::new(Vec::<u64>::new());
+                            let mut writer = BufBitWriter::<BE, _>::new(word_write);
+
+                            u64_encoding.iter().for_each(|n| { writer.write_rice(*n, param).unwrap(); } );
+                            let compressed_data = writer.into_inner().unwrap().into_inner();
+
+                            let mut bits = compressed_data.iter().flat_map(|x| {
+                                let arr: [u8; 8] = x.to_ne_bytes();
+                                let mut arr1: [u8; 4] = [0; 4];
+                                let mut arr2: [u8; 4] = [0; 4];
+                                arr1[0..4].copy_from_slice(&(arr[0..4]));
+                                arr2[0..4].copy_from_slice(&(arr[4..8]));
+                                [u32::from_ne_bytes(arr1), u32::from_ne_bytes(arr2)]
+                            }).collect::<Vec<u32>>();
+
+                            let mut bitpacked: Vec<u8> = Vec::new();
+
+                            let bitpacker = BitPacker8x::new();
+                            let block_size = 256;
+
+                            if block_size - bits.len() % block_size != 0 {
+                                bits.resize(bits.len() + (block_size - bits.len() % block_size), 0);
+                            }
+                            bits.chunks(block_size).for_each(|chunk| {
+                                let num_bits: u8 = bitpacker.num_bits(chunk);
+                                let mut compressed = vec![0u8; 4 * BitPacker8x::BLOCK_LEN];
+                                let clen = bitpacker.compress(chunk, &mut compressed[..], num_bits);
+
+                                assert_eq!(clen, 1024);
+                                assert_eq!(num_bits, 32);
+
+                                bitpacked.append(&mut compressed);
+                            });
+
+                            let block_header = BlockHeader{ block_size: bitpacked.len() as u32,
+                                                            num_records: num_records as u32,
+                                                            num_u64: u64_encoding.len() as u32,
+                                                            encoded_size: compressed_data.len() as u32,
+                                                            rice_param: param as u8,
+                                                            bitpacker_exponent: 8_u8,
+                                                            placeholder1: 0, placeholder2: 0, placeholder3: 0,
+                            };
+
+                            let nbytes = encode_into_std_write(
+                                &block_header,
+                                &mut stdout.lock(),
+                                bincode::config::standard().with_fixed_int_encoding(),
+                            );
+                            assert_eq!(nbytes.unwrap(), 32);
+                            let _ = stdout.lock().write_all(&bitpacked);
+
+                            u64_encoding.clear();
+                        }
+                        // TODO Write whatever is left in the block
                     }
-
-                    let word_write = MemWordWriterVec::new(Vec::<u64>::new());
-                    let mut writer = BufBitWriter::<BE, _>::new(word_write);
-
-                    // Best: Rice
-                    let inv_mean: f64 = ((u64_encoding.len() as f64).ln() - (u64_encoding.iter().sum::<u64>() as f64).ln()).exp();
-                    let param = dsi_bitstream::codes::rice::log2_b(inv_mean);
-
-                    u64_encoding.iter().for_each(|n| { writer.write_rice(*n, param).unwrap(); } );
-
-                    let compressed_data = writer.into_inner().unwrap().into_inner();
-
-                    let mut bits = compressed_data.iter().flat_map(|x| {
-                        let arr: [u8; 8] = x.to_ne_bytes();
-                        let mut arr1: [u8; 4] = [0; 4];
-                        let mut arr2: [u8; 4] = [0; 4];
-                        arr1[0..4].copy_from_slice(&(arr[0..4]));
-                        arr2[0..4].copy_from_slice(&(arr[4..8]));
-                        [u32::from_ne_bytes(arr1), u32::from_ne_bytes(arr2)]
-                    }).collect::<Vec<u32>>();
-
-                    let bitpacker = BitPacker8x::new();
-                    let block_size = 256;
-
-                    if block_size - bits.len() % block_size != 0 {
-                        bits.resize(bits.len() + (block_size - bits.len() % block_size), 0);
-                    }
-                    bits.chunks(block_size).for_each(|chunk| {
-                        let num_bits: u8 = bitpacker.num_bits(chunk);
-                        let mut compressed = vec![0u8; 4 * BitPacker8x::BLOCK_LEN];
-                        let _ = bitpacker.compress(chunk, &mut compressed[..], num_bits);
-
-                        let _ = encode_into_std_write(
-                            &compressed,
-                            &mut stdout.lock(),
-                            bincode::config::standard(),
-                        );
-                    });
 
                 },
             };
