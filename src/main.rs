13ee3aa88ff3d0a437860b63961e41e22668d712
Tommi Mäklin <tommi@maklin.fi>
2025-07-11 18:18:13 +0200

    Split encode.rs from main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,52 +17,22 @@ use std::path::PathBuf;
 
 use bitpacking::{BitPacker8x, BitPacker};
 use bincode::encode_into_std_write;
-use bincode::decode_from_std_read;
 use bincode::decode_from_slice;
-use bincode::{Encode, Decode};
 use clap::Parser;
-use flate2::write::GzEncoder;
 use flate2::write::GzDecoder;
-use flate2::Compression;
 use log::info;
 use needletail::Sequence;
 use needletail::parser::SequenceRecord;
 use sbwt::SbwtIndexVariant;
 use sbwt::StreamingIndex;
 
-use dsi_bitstream::impls::MemWordWriterVec;
-use dsi_bitstream::impls::BufBitWriter;
 use dsi_bitstream::traits::BE;
-use dsi_bitstream::codes::RiceWrite;
 use dsi_bitstream::prelude::MemWordReader;
 use dsi_bitstream::prelude::BufBitReader;
 use dsi_bitstream::codes::RiceRead;
 
-// Command-line interface
 mod cli;
-
-#[derive(Encode, Decode)]
-struct HeaderPlaceholder {
-    pub ph1: u64,
-    pub ph2: u64,
-    pub ph3: u64,
-    pub ph4: u64,
-}
-
-#[derive(Encode, Decode)]
-struct BlockHeader {
-    pub block_size: u32,
-    pub num_records: u32,
-    pub num_u64: u32,
-    pub encoded_size: u32,
-    pub rice_param: u8,
-    pub bitpacker_exponent: u8,
-    pub placeholder1: u64,
-    pub placeholder2: u32,
-    pub placeholder3: u16,
-    // TODO should store the used codec so others can be used
-    // TODO store total number of bases?
-}
+mod encode;
 
 /// Initializes the logger with verbosity given in `log_max_level`.
 fn init_log(log_max_level: usize) {
@@ -130,76 +100,6 @@ fn read_input_list(
     }).collect::<Vec<(String, PathBuf)>>()
 }
 
-fn encode_block(
-    u64_encoding: &[u64],
-    num_records: usize,
-) -> Vec<u8> {
-    // Best: Rice
-    let inv_mean: f64 = ((u64_encoding.len() as f64).ln() - (u64_encoding.iter().sum::<u64>() as f64).ln()).exp();
-    let param = dsi_bitstream::codes::rice::log2_b(inv_mean);
-
-    let word_write = MemWordWriterVec::new(Vec::<u64>::new());
-    let mut writer = BufBitWriter::<BE, _>::new(word_write);
-
-    u64_encoding.iter().for_each(|n| { writer.write_rice(*n, param).unwrap(); } );
-    let _ = writer.flush();
-    let compressed_data = writer.into_inner().unwrap().into_inner();
-
-    let mut bits = compressed_data.iter().flat_map(|x| {
-        let arr: [u8; 8] = x.to_ne_bytes();
-        let mut arr1: [u8; 4] = [0; 4];
-        let mut arr2: [u8; 4] = [0; 4];
-        arr1[0..4].copy_from_slice(&(arr[0..4]));
-        arr2[0..4].copy_from_slice(&(arr[4..8]));
-        [u32::from_ne_bytes(arr1), u32::from_ne_bytes(arr2)]
-    }).collect::<Vec<u32>>();
-
-    let mut bitpacked: Vec<u8> = Vec::new();
-
-    let bitpacker = BitPacker8x::new();
-    let block_size = 256;
-
-    if block_size - bits.len() % block_size != 0 {
-        bits.resize(bits.len() + (block_size - bits.len() % block_size), 0);
-    }
-    bits.chunks(block_size).for_each(|chunk| {
-        let num_bits: u8 = bitpacker.num_bits(chunk);
-        let mut compressed = vec![0u8; 4 * BitPacker8x::BLOCK_LEN];
-        let clen = bitpacker.compress(chunk, &mut compressed[..], num_bits);
-
-        assert_eq!(clen, 1024);
-        assert_eq!(num_bits, 32);
-
-        bitpacked.append(&mut compressed);
-    });
-
-    let mut deflated: Vec<u8> = Vec::with_capacity(bitpacked.len());
-    let mut encoder = GzEncoder::new(&mut deflated, Compression::default());
-    encoder.write_all(&bitpacked).unwrap();
-    encoder.finish().unwrap();
-
-    let block_header = BlockHeader{ block_size: deflated.len() as u32,
-                                    num_records: num_records as u32,
-                                    num_u64: u64_encoding.len() as u32,
-                                    encoded_size: compressed_data.len() as u32,
-                                    rice_param: param as u8,
-                                    bitpacker_exponent: 8_u8,
-                                    placeholder1: 0, placeholder2: 0, placeholder3: 0,
-    };
-
-    let mut block: Vec<u8> = Vec::with_capacity(32 + deflated.len()/8);
-
-    let nbytes = encode_into_std_write(
-        &block_header,
-        &mut block,
-        bincode::config::standard().with_fixed_int_encoding(),
-    );
-    assert_eq!(nbytes.unwrap(), 32);
-    block.append(&mut deflated);
-
-    block
-}
-
 fn main() {
     let cli = cli::Cli::parse();
 
@@ -263,7 +163,7 @@ fn main() {
             // TODO should use MB here
             let block_size = 65536;
 
-            let header_placeholder = HeaderPlaceholder{ ph1: 0, ph2: 0, ph3: 0, ph4: 0 };
+            let header_placeholder = encode::HeaderPlaceholder{ ph1: 0, ph2: 0, ph3: 0, ph4: 0 };
             let nbytes = encode_into_std_write(
                 &header_placeholder,
                 &mut stdout.lock(),
@@ -302,13 +202,13 @@ fn main() {
                         });
 
                         if u64_encoding.len() > block_size {
-                            let block = encode_block(&u64_encoding, num_records);
+                            let block = encode::encode_block(&u64_encoding, num_records);
                             let _ = stdout.lock().write_all(&block);
                             num_records = 0;
                             u64_encoding.clear();
                         }
                     }
-                    let block = encode_block(&u64_encoding, num_records);
+                    let block = encode::encode_block(&u64_encoding, num_records);
                     let _ = stdout.lock().write_all(&block);
                 },
             };
@@ -330,12 +230,12 @@ fn main() {
             // File header
             let mut header_bytes: [u8; 32] = [0_u8; 32];
             let _ = conn.read_exact(&mut header_bytes);
-            let file_header: HeaderPlaceholder = decode_from_slice(&mut header_bytes, bincode::config::standard().with_fixed_int_encoding()).unwrap().0;
+            let file_header: encode::HeaderPlaceholder = decode_from_slice(&mut header_bytes, bincode::config::standard().with_fixed_int_encoding()).unwrap().0;
 
             let mut i = 1;
 
             while let Ok(_) = conn.read_exact(&mut header_bytes) {
-                let header: BlockHeader = decode_from_slice(&mut header_bytes, bincode::config::standard().with_fixed_int_encoding()).unwrap().0;
+                let header: encode::BlockHeader = decode_from_slice(&mut header_bytes, bincode::config::standard().with_fixed_int_encoding()).unwrap().0;
                 let mut bytes: Vec<u8> = Vec::new();
                 bytes.resize(header.block_size as usize, 0_u8);
 
